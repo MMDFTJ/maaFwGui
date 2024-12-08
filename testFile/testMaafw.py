a0899522784fc7246d5b77dcaed1ad70a17dfa0b
MMDFTJ <73114181+MMDFTJ@users.noreply.github.com>
2024-12-09 00:05:21 +0800

    Update testMaafw.py

--- testFile/testMaafw.py
+++ testFile/testMaafw.py
@@ -4,10 +4,21 @@ from utils.maafw import maafw
 
 
 class TestMaaFw:
-    async def start_maafw(self):
-        print(await maafw.find_adb_devices())
+    def __init__(self):
+        self.data = None
 
+    async def start_maafw(self):
+        self.data = await maafw.find_adb_devices()
+
+
+    async def connect(self):
+        await maafw.connect_adb(path=self.data[0].adb_path, address=self.data[0].address, config=self.data[0].config)
+
+    async def click(self):
+        await maafw.click(100, 100)
 
 
 t = TestMaaFw()
 asyncio.run(t.start_maafw())
+asyncio.run(t.connect())
+asyncio.run(t.click())
